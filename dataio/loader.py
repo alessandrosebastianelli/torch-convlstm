5ecafb43b7046e80d906a25574845439c86276a7
alessandrosebastianelli <alessandroseba1995@gmail.com>
2024-01-30 13:48:20 +0000

    increasing dataset size + removing [-1, +1] normalization

--- dataio/loader.py
+++ dataio/loader.py
@@ -12,37 +12,48 @@ class CloudSimulator(Dataset):
         self.ou_lenght = ou_lenght
         self.transform = transform
 
+
+        self.OBSERVATION_WINDOWN_LENGHT  = self.in_lenght + self.ou_lenght
+        self.IMAGE_SIZE                  = (128,128)
+        self.CLOUD_SIZE_RANGE            = (int(self.IMAGE_SIZE[0]*20/100),int(self.IMAGE_SIZE[0]*60/100),int(self.IMAGE_SIZE[1]*20/100),int(self.IMAGE_SIZE[1]*60/100))
+        self.CLOUD_STARTING_POINTS_RANGE = (0,int(self.IMAGE_SIZE[0]*20/100),0,int(self.IMAGE_SIZE[1]*20/100))
+        self.CLOUD_VELOCITY_RANGE        = (1,int(self.IMAGE_SIZE[0]*2.5/100),1,int(self.IMAGE_SIZE[1]*2.5/100))
+        self.CLOUD_DIRECTIONS            = (-1,1,-1,1)
+
+
+        self.cws = np.arange(self.CLOUD_SIZE_RANGE[0], self.CLOUD_SIZE_RANGE[1])
+        self.chs = np.arange(self.CLOUD_SIZE_RANGE[2], self.CLOUD_SIZE_RANGE[3])
+
+        self.xs = np.arange(self.CLOUD_STARTING_POINTS_RANGE[0], self.CLOUD_STARTING_POINTS_RANGE[1])
+        self.ys = np.arange(self.CLOUD_STARTING_POINTS_RANGE[2], self.CLOUD_STARTING_POINTS_RANGE[3])
+
+        self.cvxs = np.arange(self.CLOUD_VELOCITY_RANGE[0], self.CLOUD_VELOCITY_RANGE[1])
+        self.cvys = np.arange(self.CLOUD_VELOCITY_RANGE[2], self.CLOUD_VELOCITY_RANGE[3])
+
+
     def __len__(self):
         return self.size
 
     def __getitem__(self, idx):
-        OBSERVATION_WINDOWN_LENGHT  = self.in_lenght + self.ou_lenght
-        IMAGE_SIZE                  = (128,128)
-        CLOUD_SIZE_RANGE            = (int(IMAGE_SIZE[0]*20/100),int(IMAGE_SIZE[0]*60/100),int(IMAGE_SIZE[1]*20/100),int(IMAGE_SIZE[1]*60/100))
-        CLOUD_STARTING_POINTS_RANGE = (0,int(IMAGE_SIZE[0]*20/100),0,int(IMAGE_SIZE[1]*20/100))
-        CLOUD_VELOCITY_RANGE        = (1,int(IMAGE_SIZE[0]*2.5/100),1,int(IMAGE_SIZE[1]*2.5/100))
-        CLOUD_DIRECTIONS            = (-1,1,-1,1)
-
         # Cloud size
-        cw = random.randint(CLOUD_SIZE_RANGE[0], CLOUD_SIZE_RANGE[1])
-        ch = random.randint(CLOUD_SIZE_RANGE[2], CLOUD_SIZE_RANGE[3])        
+        cw = random.choice(self.cws)
+        ch = random.choice(self.chs)
         # Cloud Position
-        x0  = random.randint(CLOUD_STARTING_POINTS_RANGE[0], CLOUD_STARTING_POINTS_RANGE[1])
-        y0  = random.randint(CLOUD_STARTING_POINTS_RANGE[2], CLOUD_STARTING_POINTS_RANGE[3])
+        x0 = random.choice(self.xs)
+        y0 = random.choice(self.ys)
         # Cloud velocity
-        cvx = random.randint(CLOUD_VELOCITY_RANGE[0], CLOUD_VELOCITY_RANGE[1])
-        cvy = random.randint(CLOUD_VELOCITY_RANGE[2], CLOUD_VELOCITY_RANGE[3])    
+        cvx = random.choice(self.cvxs)
+        cvy = random.choice(self.cvys)
         # Cloud direction
-        dx = random.choice([CLOUD_DIRECTIONS[0], CLOUD_DIRECTIONS[1]])
-        dy = random.choice([CLOUD_DIRECTIONS[1], CLOUD_DIRECTIONS[2]])
+        dx = random.choice([self.CLOUD_DIRECTIONS[0], self.CLOUD_DIRECTIONS[1]])
+        dy = random.choice([self.CLOUD_DIRECTIONS[1], self.CLOUD_DIRECTIONS[2]])
         # Initializing image and cloud
-        img   = np.zeros((OBSERVATION_WINDOWN_LENGHT,)+IMAGE_SIZE)
-        cloud = np.zeros(IMAGE_SIZE)
-        
+        img   = np.zeros((self.OBSERVATION_WINDOWN_LENGHT,)+self.IMAGE_SIZE)
+        cloud = np.zeros(self.IMAGE_SIZE)
         
         x = x0
         y = y0
-        for i in range(OBSERVATION_WINDOWN_LENGHT):
+        for i in range(self.OBSERVATION_WINDOWN_LENGHT):
             for xx in range(x, x+cw):
                 for yy in range(y, y+ch):
                     try:
@@ -54,7 +65,7 @@ class CloudSimulator(Dataset):
             y = y + cvy*dx
 
         
-        img = 2*img - 1
+        #img = 2*img - 1
 
         in_seq = img[:self.in_lenght]
         ou_seq = img[self.in_lenght:]
@@ -78,7 +89,7 @@ class SimulatedCloudsDataset(pl.LightningDataModule):
 
     def setup(self, stage=None):
         #transform = transforms.Compose([transforms.ToTensor()])
-        self.train_dataset = CloudSimulator(size = 1000, in_lenght=self.in_lenght, ou_lenght=self.ou_lenght)#, transform=transform)
+        self.train_dataset = CloudSimulator(size = 10000, in_lenght=self.in_lenght, ou_lenght=self.ou_lenght)#, transform=transform)
         self.valid_dataset = CloudSimulator(size = 100,  in_lenght=self.in_lenght, ou_lenght=self.ou_lenght)#, transform=transform)
         self.test_dataset  = CloudSimulator(size = 10,   in_lenght=self.in_lenght, ou_lenght=self.ou_lenght)#, transform=transform)
 
