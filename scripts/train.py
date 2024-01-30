dc6dbf0d49496eca78996de2fe2d0d38ff9fac5d
alessandrosebastianelli <alessandroseba1995@gmail.com>
2024-01-30 10:58:09 +0000

    adjusting code to fit on available resources

--- scripts/train.py
+++ scripts/train.py
@@ -13,10 +13,11 @@ from dataio.loader import SimulatedCloudsDataset
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision('high')
+    
+    input_seq  = 24
+    future_seq = 12
 
-    future_seq = 4
-
-    data_module = SimulatedCloudsDataset(batch_size=16, num_workers=4, in_lenght=8, ou_lenght=future_seq)
+    data_module = SimulatedCloudsDataset(batch_size=4, num_workers=16, in_lenght=input_seq, ou_lenght=future_seq)
     tb_logger = pl.loggers.TensorBoardLogger(os.path.join('lightning_logs','convlstm'), name='EncoderDecoderConvLSTM')
 
     # Instantiate ModelCheckpoint callback
@@ -29,7 +30,7 @@ if __name__ == "__main__":
     )
 
     # Instantiate LightningModule and DataModule
-    model = EncoderDecoderConvLSTM(nf=32, in_chan=1, future_seq=future_seq)
+    model = EncoderDecoderConvLSTM(nf=128, in_chan=1, future_seq=future_seq)
 
     # Instantiate Trainer
     trainer = pl.Trainer(max_epochs=50, callbacks=[checkpoint_callback], logger=tb_logger)
